d5e7fd06dcf10004bc1c4ff19d5cab54a4a27dee
Keshav Yadav <ydkeshav295@gmail.com>
2019-02-26 09:14:19 +0530

    tried material-ui/core components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Card from '@material-ui/core/Card';
+import { CardContent, Typography, CardActions, IconButton, Collapse } from '@material-ui/core';
+
 
 class App extends Component {
   constructor(props){
@@ -8,13 +13,29 @@ class App extends Component {
       results: [],
       loading: true,
       query: '', 
-      history: []
+      history: [], 
+      expanded: false
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleChange = this.handleChange.bind(this);
     this.handleReset = this.handleReset.bind(this);
+    this.handleExpandClick = this.handleExpandClick.bind(this);
   }
 
+
+/*
+                    <h3>Title: {data.Title}</h3>, 
+                    <h3>Actors: {data.Actors}</h3>, 
+                    <h3>Director: {data.Director}</h3>, 
+                    <h3>Box Office Collection {data.BoxOffice}</h3>
+*/
+//genre, Runtime, Writer, Year, 
+
+  handleExpandClick(e){
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  }
 
   handleClick(event){
     const data = {t: this.state.query};
@@ -30,10 +51,24 @@ class App extends Component {
         })*/
         this.setState({
           loading: false,
-          results: [<h3>Title: {data.Title}</h3>, 
-                    <h3>Actors: {data.Actors}</h3>, 
-                    <h3>Director: {data.Director}</h3>, 
-                    <h3>Box Office Collection {data.BoxOffice}</h3>]
+          results: [ 
+          <Card style={{minWidth: 275}}>
+            <CardContent>
+             
+                <h3>Title</h3>{data.Title}
+                <h3>Actors</h3><span> {data.Actors}</span>
+                <h3>Directors</h3><span>{data.Director}</span> 
+                <h3>Box Office Collection</h3> <span>{data.BoxOffice}</span>
+                <h3>Genre</h3><span> {data.Genre}</span>
+                <h3>Runtime</h3><span> {data.Runtime}</span>
+
+
+            </CardContent>
+          
+        
+          </Card>
+
+          ]
         });
       })
 
@@ -68,11 +103,11 @@ class App extends Component {
       <div className="App">
       <h2>Enter a search query!</h2>
 
-      <input type = "text" value ={this.state.query} onChange = {this.handleChange} />
+      <TextField label ='Search for a movie' margin = 'normal' type = "text" value ={this.state.query} onChange = {this.handleChange} />
       <br></br>
       <br></br>
-       <button name ='querymovie' id ="search-box" onClick = {this.handleClick}> Search</button>
-       <button onClick ={this.handleReset}>Reset</button>
+       <Button variant ="contained" color = "primary" margin = 'normal' name ='querymovie' id ="search-box" onClick = {this.handleClick}> Search</Button>
+       <Button variant ="contained" color = "secondary" margin = 'normal' onClick ={this.handleReset}>Reset</Button>
        <br></br>
        <br></br>
       {text}
